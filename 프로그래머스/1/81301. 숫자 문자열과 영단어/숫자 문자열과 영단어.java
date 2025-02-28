bb7d96be559240dbfdd8d97dbcf6a1a1510f97b4
jiyoon0000 <rlawldbs9128@gmail.com>
2025-02-28 16:33:08 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.60 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -1,29 +1,10 @@
 class Solution {
     public int solution(String s) {
-        
-        String[] words = {"zero", "one", "two", "three", "four", "five",
-                         "six", "seven", "eight", "nine"};
-        
-        StringBuilder answer = new StringBuilder();
-        StringBuilder numCard = new StringBuilder();
-        
-        for(int i = 0; i < s.length(); i++){
-            char c = s.charAt(i);
-            
-            if(Character.isDigit(c)){
-                answer.append(c);
-            } else{
-                numCard.append(c);
-                
-                for(int j = 0; j < words.length; j++){
-                    if(numCard.toString().equals(words[j])){
-                        answer.append(j);
-                        numCard.setLength(0);
-                        break;
-                    }
-                }
-            }
+        String[] strArr = {"zero", "one", "two", "three", "four", "five", 
+                           "six", "seven", "eight", "nine"};
+        for(int i = 0; i < strArr.length; i++) {
+            s = s.replaceAll(strArr[i], Integer.toString(i));
         }
-        return Integer.parseInt(answer.toString());
+        return Integer.parseInt(s);
     }
 }
