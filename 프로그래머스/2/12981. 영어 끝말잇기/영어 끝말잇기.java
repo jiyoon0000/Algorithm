e41130a30bbf6797f58e3002857604207dd5824b
jiyoon0000 <rlawldbs9128@gmail.com>
2025-03-03 02:25:10 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.19 ms, Memory: 86.5 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -1,25 +1,31 @@
-import java.util.HashSet;
+import java.util.ArrayList;
 
 class Solution {
     public int[] solution(int n, String[] words) {
+
+        ArrayList<String> duplicaition = new ArrayList<>();
         
-        HashSet<String> duplication = new HashSet<>();
-        duplication.add(words[0]);
+        duplicaition.add(words[0]);
         
-        for(int i = 1; i < words.length; i++) {
-            char last = words[i - 1].charAt(words[i - 1].length() - 1);
-            char first = words[i].charAt(0);
+        for (int i = 1; i < words.length; i++) {
+            String prevWord = words[i - 1];
+
+            String currWord = words[i];
             
-            if(first != last || duplication.contains(words[i])) { 
+            char last = prevWord.charAt(prevWord.length() - 1);
+            char first = currWord.charAt(0);
+            
+            if (first != last || duplicaition.contains(currWord)) {
+
                 int human = (i % n) + 1;
                 int turn = (i / n) + 1;
                 
-                return new int[]{human, turn};
+                return new int[] {human, turn};
             }
             
-            duplication.add(words[i]);
+            duplicaition.add(currWord);
         }
-
-        return new int[]{0,0};
+        
+        return new int[] {0, 0};
     }
 }
