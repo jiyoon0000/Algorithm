1552f914740ebf3f7209e78b92221db822c985b2
jiyoon0000 <rlawldbs9128@gmail.com>
2025-03-26 14:54:28 +0900

    [level 1] Title: 체육복, Time: 0.03 ms, Memory: 85.9 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -1,35 +1,39 @@
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        int[] clothes = new int[n + 1];
-        
-        for (int i = 1; i <= n; i++) {
-            clothes[i] = 1;
-        }
+        boolean[] lostStudent = new boolean[n + 1];
+        boolean[] reserveStudent = new boolean[n + 1];
         
         for (int l : lost) {
-            clothes[l]--;
+            lostStudent[l] = true;
         }
         
         for (int r : reserve) {
-            clothes[r]++;
+            reserveStudent[r] = true;
         }
         
         for (int i = 1; i <= n; i++) {
-            if (clothes[i] == 0) {
-                if (i - 1 >= 1 && clothes[i - 1] == 2) {
-                    clothes[i - 1] = 1;
-                    clothes[i] = 1;
+            if (lostStudent[i] && reserveStudent[i]) {
+                lostStudent[i] = false;
+                reserveStudent[i] = false;
+            }
+        }
+        
+        for (int i = 1; i <= n; i++) {
+            if (lostStudent[i]) {
+                if (i - 1 >= 1 && reserveStudent[i - 1]) {
+                    reserveStudent[i - 1] = false;
+                    lostStudent[i] = false;
                 }
-                else if (i + 1 <= n && clothes[i + 1] == 2) {
-                    clothes[i + 1] = 1;
-                    clothes[i] = 1;
+                else if (i + 1 <= n && reserveStudent[i + 1]) {
+                    reserveStudent[i + 1] = false;
+                    lostStudent[i] = false;
                 }
             }
         }
         
         int answer = 0;
         for (int i = 1; i <= n; i++) {
-            if (clothes[i] >= 1) {
+            if (!lostStudent[i]) {
                 answer++;
             }
         }
